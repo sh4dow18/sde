4c2b9f965dad80a1d6858d1150407022584bd3ac
Ramses Solano Arias <ramses.solano.arias@gmail.com>
2021-03-13 16:45:53 -0600

    Update lightdm_instalation.sh

--- lightdm_instalation.sh
+++ lightdm_instalation.sh
@@ -2,4 +2,4 @@ sudo pacman -Syu
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server
 sudo cp settings/lightdm.conf /etc/lightdm/
 sudo systemctl enable lightdm
-echo "\n\nInstalacion Completada :D\n\n"
+echo -e "\n\nInstalacion Completada :D\n\n"
