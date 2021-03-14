4d0b9c660befe7753a0da229ec78683067d6bf1a
Ramses Solano Arias <ramses.solano.arias@gmail.com>
2021-03-13 22:30:54 -0600

    Instalación Prueba 2

--- installer.sh
+++ installer.sh
@@ -2,19 +2,20 @@
 sudo pacman -Syu
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server firefox alacritty code qtile rofi sed which feh picom
 
-
+USER=/home/$1
 echo -e "\n\nCargando Configuraciones...\n\n"
 sleep 5
+
 
 sudo cp settings/lightdm/lightdm.conf /etc/lightdm/
 sudo systemctl enable lightdm
 
-sudo mkdir ~/.config
-sudo cp -r settings/alacritty ~/.config/
-sudo mkdir ~/backgrounds
-sudo cp settings/start/Gracias.png ~/backgrounds/
-sudo cp settings/start/.xsessions ~/
-sudo chmod u+x ~/.xsessions
+sudo mkdir $USER/.config
+sudo cp -r settings/alacritty $USER/.config/
+sudo mkdir $USER/backgrounds
+sudo cp settings/start/Gracias.png $USER/backgrounds/
+sudo cp settings/start/.xsessions $USER/
+sudo chmod u+x $USER/.xsessions
 
 
 echo -e "\n\nInstalacion Completada :D\n\n"
---
+++ uninstaller.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+sudo pacman -Rs lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server firefox alacritty code qtile rofi which feh picom
+
+echo -e "\n\nDesinstalacion Completada :D\n\n"
