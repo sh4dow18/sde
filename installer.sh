e124aca340a7a05890d08990eaaece9b995f2946
Ramses Solano Arias <ramses.solano.arias@gmail.com>
2021-03-14 23:42:53 -0600

    Agregando Configuración de Qtile y Mejorando Instalador y Desinstalador

--- installer.sh
+++ installer.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
+
+# Created by Cyb3rHunter
+# My Github: https://www.github.com/Cyb3rHunter
+# My Gaming, Reactions and Computing Spanish Channel: https://www.youtube.com/RamgiD
+
+# Installing System Updates
 sudo pacman -Syu
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server firefox alacritty code qtile rofi sed which feh picom
 
+# Installing Programs
+sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server qtile alacritty rofi sed which feh firefox code picom gimp
+
+# Creating User Home's Variable
 USER=/home/$1
-echo -e "\n\nCargando Configuraciones...\n\n"
-sleep 5
 
-
+# Installing Lightdm Configuration
 sudo cp settings/lightdm/lightdm.conf /etc/lightdm/
 sudo systemctl enable lightdm
 
+# Creating Configuration Directory
 sudo mkdir $USER/.config
+
+# Installing Qtile Configuration
+cp -r settings/qtile $USER/.config/
+
+# Installing Alacritty Configuration
 sudo cp -r settings/alacritty $USER/.config/
+
+# Creating Backgrounds Directory
 sudo mkdir $USER/backgrounds
-sudo cp settings/start/Gracias.png $USER/backgrounds/
-sudo cp settings/start/.xsession $USER/
+
+# Installing Thank You for Install Background
+cp settings/start/Gracias.png $USER/backgrounds/
+
+# Installing Startup Configuration File (.xsession)
+cp settings/start/.xsession $USER/
 sudo chmod u+x $USER/.xsession
 
+# Changing the new files owner
 sudo chown -R $1:$1 $USER/.config
 sudo chown -R $1:$1 $USER/backgrounds
 sudo chown $1:$1 $USER/.xsession
 
-
-
-echo -e "\n\nInstalacion Completada :D\n\n"
+# Saying that the Installation has finished
+echo -e "\n\nInstalation Complete :D\n\n"
--- uninstaller.sh
+++ uninstaller.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# Created by Cyb3rHunter
+# My Github: https://www.github.com/Cyb3rHunter
+# My Gaming, Reactions and Computing Spanish Channel: https://www.youtube.com/RamgiD
+
+# Removing Lightdm Configuration
 sudo systemctl disable lightdm
 sudo rm -r /etc/lightdm/
 
-sudo pacman -Rs lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server firefox alacritty code qtile rofi which feh picom
+# Uninstalling Programs
+sudo pacman -Rs lightdm lightdm-gtk-greeter lightdm-gtk-greeter xorg-server qtile alacritty rofi sed which feh firefox code picom gimp
 
+# Creating User Home's Variable
 USER=/home/$1
+
+# Removing Config and Backgrounds Directories, also Startup Configuration File(.xsession)
 sudo rm -r $USER/.config $USER/backgrounds $USER/.xsession
 
+# Saying that the Uninstall has Finished
 echo -e "\n\nDesinstalacion Completada :D\n\n"
