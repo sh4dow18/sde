7030e5728d3a00c2696ac36b9685ae7d6ce7008c
Ramses Solano Arias <ramses.solano.arias@gmail.com>
2021-03-24 22:58:58 -0600

    Adding Esthetic to Installer

--- installer.sh
+++ installer.sh
@@ -1,24 +1,100 @@
 #!/bin/bash
 
-# Created By: Cyb3rHunter
-# My Github: https://github.com/Cyb3rHunter
-# My Reactions, Gaming and Computing Spanish Youtube Channel: https://youtube.com/c/RamgiD
+clear
+echo -e "\e[1;33m		                                        ____   "
+echo -e "\e[1;33m		    .--.--.              .---.        ,'  , \`." 
+echo -e "\e[1;33m		   /  /    '.           /. ./|     ,-+-,.' _ | "
+echo -e "\e[1;33m		  |  :  /\`. /       .--'.  ' ;  ,-+-. ;   , || "
+echo -e "\e[1;33m		  ;  |  |--\`       /__./ \\ : | ,--.'|'   |  ;| "
+echo -e "\e[1;33m		  |  :  ;_     .--'.  '   \\' .|   |  ,', |  ': "
+echo -e "\e[1;33m		   \\  \\    \`. /___/ \\ |    ' '|   | /  | |  || "
+echo -e "\e[1;33m		    \`----.   \\;   \\  \\;      :'   | :  | :  |, "
+echo -e "\e[1;33m		    __ \\  \\  | \\   ;  \`      |;   . |  ; |--'  "
+echo -e "\e[1;33m		   /  /\`--'  /  .   \\    .\\  ;|   : |  | ,     "
+echo -e "\e[1;33m		  '--'.     /    \\   \\   ' \\ ||   : '  |/      "
+echo -e "\e[1;33m		    \`--'---'      :   '  |--\" ;   | |\`-'       "
+echo -e "\e[1;33m		                   \\   \\ ;    |   ;/           "
+echo -e "\e[1;33m		                    '---\"     '---'            "
+
+echo -e "\e[1;32m     _____                _           _   _"
+echo -e "\e[1;32m    / ____|              | |         | | | |            _" 
+echo -e "\e[1;32m   | |     _ __ ___  __ _| |_ ___  __| | | |__  _   _  (_)"
+echo -e "\e[1;32m   | |    | '__/ _ \\/ _\` | __/ _ \\/ _\` | | '_ \\| | | |"
+echo -e "\e[1;32m   | |____| | |  __/ (_| | ||  __/ (_| | | |_) | |_| |  _" 
+echo -e "\e[1;32m    \\_____|_|  \\___|\\__,_|\\__\\___|\\__,_| |_.__/ \\__, | (_)"
+echo -e "\e[1;32m                                                 __/ |"
+echo -e "\e[1;32m                                                |___/ "                                                                             
+
+echo -e "\e[1;36m    ______   __      _    _          __                    __      ____    "
+echo -e "\e[1;36m  .' ____ \\ [  |    | |  | |        |  ]                  /  |   .' __ '.  "
+echo -e "\e[1;36m  | (___ \\_| | |--. | |__| |_   .--.| |  .--.   _   _   __\`| |   | (__) |  "
+echo -e "\e[1;36m   _.____\`.  | .-. ||____   _|/ /'\`\\' |/ .'\`\\ \\[ \\ [ \\ [  ]| |   .\`____'.  "
+echo -e "\e[1;36m  | \\____) | | | | |    _| |_ | \__/  || \__. | \\ \\/\\ \\/ /_| |_ | (____) | "
+echo -e "\e[1;36m   \\______.'[___]|__]  |_____| '.__.;__]'.__.'   \\__/\\__/|_____|\`.______.' "
+
+echo -e "\e[1;32m\n\n  My Github where I will Create				My Gaming, Reactions and Computing"
+echo -e "\e[1:32m  more Tools for Different Areas			Spanish Youtube Channel:"
+echo -e "\e[1;33m  https://github.com/sh4dow18				https://youtube.com/c/RamgiD\n"
+echo -e "\e[1;31m\e[1;1m\nLoading..."
+sleep 7
+
+clear
+echo -e "\e[1;32m   ____         __       _____             __  __        __     __        "
+echo -e "\e[1;32m  /  _/__  ___ / /____ _/ / (_)__  ___ _  / / / /__  ___/ /__ _/ /____ ___"
+echo -e "\e[1;32m _/ // _ \\(_-</ __/ _ \`/ / / / _ \\/ _ \`/ / /_/ / _ \\/ _  / _ \`/ __/ -_|_-<"
+echo -e "\e[1;32m/___/_//_/___/\\__/\\_,_/_/_/_/_//_/\\_, /  \\____/ .__/\\_,_/\\_,_/\\__/\\__/___/"
+echo -e "\e[1;32m                                 /___/       /_/                          "
+
+echo -e "\e[1;33m\n\nLooking for System Updates..."
+sleep 5
 
 # Installing System Updates
 sudo pacman -Syu
 
+echo -e "\e[1;31m\e[1;1m\nLoading..."
+sleep 4
+
+clear
+echo -e "\e[1;32m   ____         __       _____             ___                                 "
+echo -e "\e[1;32m  /  _/__  ___ / /____ _/ / (_)__  ___ _  / _ \\_______  ___ ________ ___ _  ___"
+echo -e "\e[1;32m _/ // _ \\(_-</ __/ _ \`/ / / / _ \\/ _ \`/ / ___/ __/ _ \\/ _ \`/ __/ _ \`/  ' \(_-<"
+echo -e "\e[1;32m/___/_//_/___/\\__/\\_,_/_/_/_/_//_/\\_, / /_/  /_/  \\___/\\_, /_/  \\_,_/_/_/_/___/"
+echo -e "\e[1;32m                                 /___/                /___/                    "
+
+echo -e "\e[1;36m\n\nLooking for the Necessary Programs..."
+sleep 5
+
 # Installing Programs
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xorg-server qtile alacritty rofi sed which feh firefox code picom gimp udiskie ntfs-3g xorg-xrandr vlc imv scrot thunar zip unzip xcb-util-cursor pacman-contrib openbox tint2 obconf exa fish
+
+echo -e "\e[1;31m\e[1;1m\nLoading..."
+sleep 4
+
+clear
+echo -e "\e[1;32m   ____         __       _____               "                            
+echo -e "\e[1;32m  /  _/__  ___ / /____ _/ / (_)__  ___ _      "                           
+echo -e "\e[1;32m _/ // _ \\(_-</ __/ _ \`/ / / / _ \\/ _ \`/      "                           
+echo -e "\e[1;32m/___/_//_/___/\\__/\\_,_/_/_/_/_//_/\\_, /       "                           
+echo -e "\e[1;32m  _____          ____            /___/   __  _             _____ __      "
+echo -e "\e[1;32m / ___/__  ___  / _(_)__ ___ _________ _/ /_(_)__  ___    / __(_) /__ ___"
+echo -e "\e[1;32m/ /__/ _ \\/ _ \\/ _/ / _ \`/ // / __/ _ \`/ __/ / _ \\/ _ \\  / _// / / -_|_-<"
+echo -e "\e[1;32m\\___/\\___/_//_/_//_/\\_, /\\_,_/_/  \\_,_/\\__/_/\\___/_//_/ /_/ /_/_/\\__/___/"
+echo -e "\e[1;32m                   /___/                                                 "
+
+echo -e "\e[1;33m\n\nAdding Configuration..."
+sleep 5
 
 # Creating User Home's Variable
 USER=/home/$1
 
-# Installing Lightdm Configuration
+echo -e "\e[1;31m\n	- Installing Lightdm Configuration"
 sudo cp settings/lightdm/lightdm.conf /etc/lightdm/
 sudo systemctl enable lightdm
 
 # Creating Configuration Directory
 sudo mkdir $USER/.config
+
+echo -e "\e[1;32m\n	- Installing Qtile and Openbox Configuration"
 
 # Installing Qtile Configuration
 cp -r settings/qtile $USER/.config/
@@ -27,14 +103,32 @@ cp -r settings/qtile $USER/.config/
 cp -r settings/OpenBox/RookieMode/openbox/ $USER/.config/
 cp -r settings/OpenBox/RookieMode/tint2/ $USER/.config/
 
+echo -e "\e[1;33m\n	- Installing Alacritty Configuration"
+
 # Installing Alacritty Configuration
 sudo cp -r settings/alacritty $USER/.config/
 
-# Creating Backgrounds Directory
-sudo mkdir $USER/backgrounds
+echo -e "\e[1;34m\n	- Installing Rofi Configuration"
 
-# Installing Thank You for Install Background
-cp settings/start/Thanks.png $USER/backgrounds/
+# Installing Rofi Menu Configuration
+sudo cp settings/rofi/onedark.rasi /usr/share/rofi/themes/
+mkdir $USER/.config/rofi
+cp settings/rofi/config $USER/.config/rofi/
+
+echo -e "\e[1;35m\n	- Installing Fish Configuration"
+
+# Changing to the "Fish" shell and Installing "Fish" Configuration
+sudo chsh -s /bin/fish $1
+cp -r settings/fish $USER/.config
+
+echo -e "\e[1;36m\n	- Installing New Cursor"
+
+# Installing New Cursor
+sudo tar -xf settings/gtk/cursor/Breeze.tar.gz
+sudo mv Breeze/ /usr/share/icons/
+sudo cp settings/gtk/cursor/index.theme /usr/share/icons/default/
+
+echo -e "\e[1;36m\n	- Installing Startup Files"
 
 # Installing Startup Configuration File (.xsession)
 cp settings/start/.xsession $USER/
@@ -44,10 +138,13 @@ sudo chmod u+x $USER/.xsession
 cp settings/start/.xrandr_monitor.sh $USER/
 sudo chmod u+x $USER/.xrandr_monitor.sh
 
-# Installing Rofi Menu Configuration
-sudo cp settings/rofi/onedark.rasi /usr/share/rofi/themes/
-mkdir $USER/.config/rofi
-cp settings/rofi/config $USER/.config/rofi/
+echo -e "\e[1;31m\n	- Installing Final Files..."
+
+# Creating Backgrounds Directory
+sudo mkdir $USER/backgrounds
+
+# Installing Thank You for Install Background
+cp settings/start/Thanks.png $USER/backgrounds/
 
 # Installing Bash Configuation File
 cp settings/start/.bashrc $USER/
@@ -63,11 +160,6 @@ cp settings/start/.bashrc $USER/
 # mkdir $USER/.config/gtk-3.0
 # cp settings/gtk/install/settings.ini $USER/.config/gtk-3.0/
 
-# Installing New Cursor
-sudo tar -xf settings/gtk/cursor/Breeze.tar.gz
-sudo mv Breeze/ /usr/share/icons/
-sudo cp settings/gtk/cursor/index.theme /usr/share/icons/default/
-
 # Changing the new files owner
 sudo chown -R $1:$1 $USER/.config
 sudo chown -R $1:$1 $USER/backgrounds
@@ -78,9 +170,25 @@ sudo chown $1:$1 $USER/.xrandr_monitor.sh
 # sudo chown $1:$1 $USER/gtkrc-2.0
 sudo chown $1:$1 /usr/share/rofi/themes/onedark.rasi
 
-# Changing to the "Fish" shell and Installing "Fish" Configuration
-sudo chsh -s /bin/fish $1
-cp -r settings/fish $USER/.config
+clear
+echo -e "\e[1;32m _____           _        _       _   _                _____                      _      _       "
+echo -e "\e[1;32m|_   _|         | |      | |     | | (_)              / ____|                    | |    | |      "
+echo -e "\e[1;32m  | |  _ __  ___| |_ __ _| | __ _| |_ _  ___  _ __   | |     ___  _ __ ___  _ __ | | ___| |_ ___ "
+echo -e "\e[1;32m  | | | '_ \\/ __| __/ _\` | |/ _\` | __| |/ _ \\| '_ \\  | |    / _ \\| '_ \` _ \\| '_ \| |/ _ \\ __/ _ \\"
+echo -e "\e[1;32m  | |_| | | \\__ \\ || (_| | | (_| | |_| | (_) | | | | | |___| (_) | | | | | | |_) | |  __/ ||  __/"
+echo -e "\e[1;32m|_____|_| |_|___/\\__\\__,_|_|\\__,_|\\__|_|\\___/|_| |_|  \\_____\\___/|_| |_| |_| .__/|_|\\___|\\__\\___|"
+echo -e "\e[1;32m                                                                           | |                   "
+echo -e "\e[1;32m                                                                           |_|                   "
+echo -e "\e[1;33m                                       ..::''''::..					"
+echo -e "\e[1;33m                                     .;''        \`\`;.					"
+echo -e "\e[1;33m                                    ::    ::  ::    ::					"
+echo -e "\e[1;33m                                   ::     ::  ::     ::					"
+echo -e "\e[1;33m                                   :: .:' ::  ::  :. ::					"
+echo -e "\e[1;33m                                   ::  :          :  ::					"
+echo -e "\e[1;33m                                    :: \`:.      .:' ::					"
+echo -e "\e[1;33m                                     \`;..\`\`::::''..;'				"
+echo -e "\e[1;33m                                       \`\`::,,,,::''					"
 
-# Saying that the Installation has finished
-echo -e "\n\nInstalation Completed :D\n\n"
+echo -e "\e[1;37mThe system needs to be rebooted"
+echo -e "\e[1;37m¿Do you want to reboot your system now?"
+sleep 5
