ae9b7908f97c07148cf10f5cd2203540bb1d6d7f
Ramses Solano Arias <ramses.solano.arias@gmail.com>
2021-10-25 21:58:08 -0600

    Changing User Obtaining

--- installer.sh
+++ installer.sh
@@ -39,40 +39,6 @@ echo -e "\e[1;33m  https://github.com/sh4dow18				Sh4dow18 Proyect\n"
 echo -e "\e[1;31m\e[1;1m\nLoading..."
 sleep 7
 
-clear
-echo -e "\e[1;32m  __  __              _   __        _ ____          __  _         "
-echo -e "\e[1;32m / / / /__ ___ ____  | | / /__ ____(_) _(_)______ _/ /_(_)__  ___ "
-echo -e "\e[1;32m/ /_/ (_-</ -_) __/  | |/ / -_) __/ / _/ / __/ _ \`/ __/ / _ \\/ _ \\"
-echo -e "\e[1;32m\\____/___/\\__/_/     |___/\\__/_/ /_/_//_/\\__/\\_,_/\\__/_/\\___/_//_/"
-
-echo -e "\e[1;33m\nPlease Enter the name of the user to whom you want to"
-echo -e "\e[1;33minstall the window manager with the settings\n"
-
-echo -e "\e[1;37mUsername:"
-read user
-
-verify=$(sudo cat /etc/passwd | grep $user | cut -d ':' -f 1)
-
-invalid_options="root bin daemon mail ftp http nobody dbus systemd-journal-remote systemd-network systemd-resolve systemd-timesync systemd-coredump uuidd polkitd git"
-
-if [[ $invalid_options =~ (^| )$verify($| ) ]]; then
-	echo -e "\e[1;31m\nThe installer can not use a pre installed user"
-	echo -e "\e[1;31mShutting Down the Installer"
-	echo -e "\e[1;37m		   "
-	sleep 2
-	exit 1
-
-elif [[ $verify != $user ]]; then
-	echo -e "\e[1;31m\nYou have entered an invalid username"
-	echo -e "\e[1;31mShutting Down the Installer..."
-	echo -e "\e[1;37m                  "
-	sleep 2
-	exit 1
-else
-	echo -e "\e[1;32m\n$user is a valid username"
-	sleep 2
-fi
-
 echo -e "\e[1;31m\e[1;1m\nLoading..."
 sleep 4
 
@@ -127,6 +93,7 @@ echo -e "\e[1;36m\n\nAdding Configuration..."
 sleep 5
 
 # Creating User Home's Variable
+user=$(who | cut -d " " -f 1)
 HOME=/home/$user
 
 # Creating Configuration Directory
--- uninstaller.sh
+++ uninstaller.sh
@@ -61,43 +61,6 @@ echo -e "\e[1;31m\e[1;1m\nLoading..."
 sleep 5
 
 clear
-echo -e "\e[1;32m  __  __              _   __        _ ____          __  _         "
-echo -e "\e[1;32m / / / /__ ___ ____  | | / /__ ____(_) _(_)______ _/ /_(_)__  ___ "
-echo -e "\e[1;32m/ /_/ (_-</ -_) __/  | |/ / -_) __/ / _/ / __/ _ \`/ __/ / _ \\/ _ \\"
-echo -e "\e[1;32m\\____/___/\\__/_/     |___/\\__/_/ /_/_//_/\\__/\\_,_/\\__/_/\\___/_//_/"
-
-echo -e "\e[1;33m\nPlease Enter the name of the user to whom you want to"
-echo -e "\e[1;33muninstall the window manager with the settings\n"
-
-echo -e "\e[1;37mUsername:"
-read user
-
-verify=$(sudo cat /etc/passwd | grep $user | cut -d ':' -f 1)
-
-invalid_options="root bin daemon mail ftp http nobody dbus systemd-journal-remote systemd-network systemd-resolve systemd-timesync systemd-coredump uuidd polkitd git"
-
-if [[ $invalid_options =~ (^| )$verify($| ) ]]; then
-	echo -e "\e[1;31m\nThe installer can not use a pre installed user"
-	echo -e "\e[1;31mShutting Down the Installer"
-	echo -e "\e[1;37m		   "
-	sleep 2
-	exit 1
-
-elif [[ $verify != $user ]]; then
-	echo -e "\e[1;31m\nYou have entered an invalid username"
-	echo -e "\e[1;31mShutting Down the Installer..."
-	echo -e "\e[1;37m                  "
-	sleep 2
-	exit 1
-else
-	echo -e "\e[1;32m\n$user is a valid username"
-	sleep 2
-fi
-
-echo -e "\e[1;31m\e[1;1m\nLoading..."
-sleep 4
-
-clear
 echo -e "\e[1;32m   ___                        _             __  __     _          __       ____       "
 echo -e "\e[1;32m  / _ \\_______ ___  ___ _____(_)__  ___ _  / / / /__  (_)__  ___ / /____ _/ / /__ ____"
 echo -e "\e[1;32m / ___/ __/ -_) _ \\/ _ \`/ __/ / _ \\/ _ \`/ / /_/ / _ \\/ / _ \\(_-</ __/ _ \`/ / / -_) __/"
@@ -115,6 +78,7 @@ sudo rm -r /etc/lightdm/
 sudo rm -r /usr/share/xsessions/
 
 # Creating User Home Variable
+user=$(who | cut -d " " -f 1)
 HOME=/home/$user
 
 sleep 3
